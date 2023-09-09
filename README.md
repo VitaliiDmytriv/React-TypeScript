8ed3ee5de617050651e7272d4c2c7321dd5d2b4f
Vitalii Dmytriv <vitaliqq.dmutriv@gmail.com>
2023-09-09 10:56:20 +0300

    Prop-drilling

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-Create README.md
+### Prop-drilling
+
+The key point in this section is that it is fine to share state across a few adjacent components using props but isn’t ideal for sharing across lots of components far apart in the component tree.
+
+### React context
---
+++ myapp/src/prop-drilling/App.tsx
@@ -0,0 +1,75 @@
+import { useReducer } from 'react';
+import { Header } from './Header';
+import { Main } from './Main';
+import { User, authenticate } from './api/authenticate';
+import { authorize } from './api/authorize';
+
+type State = {
+    user: undefined | User;
+    permissions: undefined | string[];
+    loading: boolean;
+};
+
+const initialState: State = {
+    user: undefined,
+    permissions: undefined,
+    loading: false,
+};
+
+type Action =
+    | {
+          type: 'authenticate';
+      }
+    | {
+          type: 'authenticated';
+          user: User | undefined;
+      }
+    | {
+          type: 'authorize';
+      }
+    | {
+          type: 'authorized';
+          permissions: string[];
+      };
+
+function reducer(state: State, action: Action): State {
+    switch (action.type) {
+        case 'authenticate':
+            return { ...state, loading: true };
+        case 'authenticated':
+            return { ...state, loading: false, user: action.user };
+        case 'authorize':
+            return { ...state, loading: true };
+        case 'authorized':
+            return { ...state, loading: false, permissions: action.permissions };
+        default:
+            return state;
+    }
+}
+
+function App() {
+    const [{ user, loading, permissions }, dispatch] = useReducer(reducer, initialState);
+
+    async function handleSignInClick() {
+        dispatch({ type: 'authenticate' });
+        const authenticateUser = await authenticate();
+        dispatch({ type: 'authenticated', user: authenticateUser });
+        if (authenticateUser) {
+            dispatch({ type: 'authorize' });
+            const authorizedPermissions = await authorize(authenticateUser.id);
+            dispatch({
+                type: 'authorized',
+                permissions: authorizedPermissions,
+            });
+        }
+    }
+
+    return (
+        <div className="max-w-7xl mx-auto px-4">
+            <Header user={user} onSignInClick={handleSignInClick} loading={loading} />
+            <Main user={user} permissions={permissions} />
+        </div>
+    );
+}
+
+export default App;
